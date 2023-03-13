1571f9c490900bc9f06c5c1f1904a5c396ccd3b9
29569 <2956901631@qq.com>
2023-03-13 17:23:39 +0800

    start和loading场景搭建

--- Assets/Scripts/StartUIManager.cs
+++ Assets/Scripts/StartUIManager.cs
@@ -7,9 +7,9 @@ public class StartUIManager : MonoBehaviour
     public static StartUIManager Instance;
 
     public Transform m_LoginPanel, m_RegisterPanel, m_MainPanel;
-    public GameObject m_HintMessage;
+    public Transform m_HintMessage;
 
-    private InputField username_Login, age_Login, username_Register, age_Register;
+    private InputField username_Login, password_Login, username_Register, password_Register;
 
     private void Awake()
     {
@@ -21,25 +21,25 @@ public class StartUIManager : MonoBehaviour
     //初始化输入框
     private void InitIF()
     {
-        username_Login = m_LoginPanel.Find("UserName/InputField").GetComponent<InputField>();
-        age_Login = m_LoginPanel.Find("Age/InputField").GetComponent<InputField>();
-        username_Register = m_RegisterPanel.Find("UserName/InputField").GetComponent<InputField>();
-        age_Register = m_RegisterPanel.Find("Age/InputField").GetComponent<InputField>();
+        username_Login = m_LoginPanel.Find("Username/InputField").GetComponent<InputField>();
+        password_Login = m_LoginPanel.Find("Password/InputField").GetComponent<InputField>();
+        username_Register = m_RegisterPanel.Find("Username/InputField").GetComponent<InputField>();
+        password_Register = m_RegisterPanel.Find("Password/InputField").GetComponent<InputField>();
     }
 
     private void ShowHint(string str)
     {
-        m_HintMessage.SetActive(true);
-        m_HintMessage.GetComponent<Text>().text = str;
+        m_HintMessage.gameObject.SetActive(true);
+        m_HintMessage.GetChild(0).GetComponent<Text>().text = str;
     }
     
     //初始化输入框的内容
     private void InitContent()
     {
         username_Login.text = "";
-        age_Login.text = "";
+        password_Login.text = "";
         username_Register.text = "";
-        age_Register.text = "";
+        password_Register.text = "";
     }
     
     //使用委托初始化按钮点击事件
@@ -50,7 +50,7 @@ public class StartUIManager : MonoBehaviour
         {
             //登陆界面的登录按钮
             //发送数据之前，客户端自己先做一个校验，以便要提交的数据不为空
-            if (username_Login.text.Equals("") || age_Login.text.Equals(""))
+            if (username_Login.text.Equals("") || password_Login.text.Equals(""))
             {    
                 InitContent();
                 ShowHint("输入的内容不能为空，请重新输入");
@@ -68,12 +68,12 @@ public class StartUIManager : MonoBehaviour
             m_RegisterPanel.gameObject.SetActive(true);
         });
         //注销
-        m_MainPanel.Find("EnterGameBtn").GetComponent<Button>().onClick.AddListener(() =>
+        m_MainPanel.Find("EntergameBtn").GetComponent<Button>().onClick.AddListener(() =>
         {
             //进入游戏
             SceneManager.LoadScene("Game1");
         });
-        m_MainPanel.Find("LogouBtn").GetComponent<Button>().onClick.AddListener(() =>
+        m_MainPanel.Find("LogoutBtn").GetComponent<Button>().onClick.AddListener(() =>
         {
             //TODO:注销登录
             m_MainPanel.gameObject.SetActive(false);
@@ -91,7 +91,7 @@ public class StartUIManager : MonoBehaviour
         {
             //注册界面的注册按钮
             //发送数据之前，客户端自己先做一个校验，以便要提交的数据不为空
-            if (username_Register.text.Equals("") || age_Register.text.Equals(""))
+            if (username_Register.text.Equals("") || password_Register.text.Equals(""))
             {
                 InitContent();
                 ShowHint("输入的内容不能为空，请重新输入");
@@ -103,7 +103,7 @@ public class StartUIManager : MonoBehaviour
         });
         m_RegisterPanel.Find("ReturnBtn").GetComponent<Button>().onClick.AddListener(() =>
         {
-            //登陆界面的返回按钮
+            //登录界面的返回按钮
             m_RegisterPanel.gameObject.SetActive(false);
             m_LoginPanel.gameObject.SetActive(true);
         });
