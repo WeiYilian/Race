5cab9a94c9bc2d2473a095eb416989acfba96661
aiyuan <1455881522@qq.com>
2023-03-14 16:55:47 +0800

    2

---
+++ Assets/Scripts/Controller/BoxZoom.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxZoom : MonoBehaviour
+{
+    
+    //缩放比例限制
+    public float MinScale = 0.02f;
+    public float MaxScale = 30f;
+    //缩放比例
+    private float scale = 1f;
+
+    void Update()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") != 0)
+        {
+            float mouseWheelValue = Input.GetAxis("Mouse ScrollWheel");
+            scale += mouseWheelValue;
+            scale = Mathf.Clamp(scale, MinScale, MaxScale);
+
+            gameObject.transform.localScale = new Vector3(scale, scale, scale);
+        }
+
+    }
+}
