db039eb78c6b4e03ddc93cba47dfaae1a6dc51c2
aiyuan <1455881522@qq.com>
2023-03-17 20:43:05 +0800

    3月17song

---
+++ Assets/Scripts/TestScript/CubeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    public float rotateSpeed = 10f;  // 控制旋转速度
+    public float scaleSpeed = 0.1f;  // 控制缩放速度
+
+    void Update()
+    {
+        // 通过鼠标移动控制Cube旋转
+        transform.Rotate(Vector3.up, Input.GetAxis("Mouse X") * rotateSpeed);
+
+        // 通过鼠标滚轮控制Cube缩放
+        float scaleFactor = 1f + Input.GetAxis("Mouse ScrollWheel") * scaleSpeed;
+        transform.localScale *= scaleFactor;
+    }
+}
---
+++ Assets/Scripts/TestScript/ItemController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemController : MonoBehaviour
+{
+    public Transform itemManager;  // 物品所属的Item Manager对象
+    private float distance = 10f;  // 物品距离Cube的最小距离
+    private bool isDragging = false;  // 是否正在拖拽
+    private Vector3 offset = Vector3.zero;  // 鼠标拖拽偏移量
+
+    void OnMouseDown()
+    {
+        isDragging = true;
+
+        // 计算鼠标拖拽偏移量
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = distance;
+        offset = Camera.main.WorldToScreenPoint(transform.position) - mousePos;
+
+        // 将物品从原来的面中移除
+        transform.parent = itemManager;
+    }
+
+    void OnMouseUp()
+    {
+        isDragging = false;
+
+        // 将物品吸附到最近的面上
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.forward, out hit, distance))
+        {
+            transform.parent = hit.collider.transform;
+            transform.localPosition = Vector3.zero;
+            Debug.Log("物品被吸附在 " + hit.collider.gameObject.name + " 上");
+        }
+    }
+
+    void Update()
+    {
+        if (isDragging)
+        {
+            // 计算鼠标拖拽偏移量
+            Vector3 mousePos = Input.mousePosition;
+            mousePos.z = distance;
+            Vector3 targetPos = Camera.main.ScreenToWorldPoint(mousePos + offset);
+
+            // 移动物品到目标位置
+            transform.position = targetPos;
+        }
+    }
+}
