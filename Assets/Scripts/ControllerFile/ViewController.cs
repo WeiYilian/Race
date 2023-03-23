bffb89c22eb844227737f11be0775a1c2a2d2d79
WeiYilian <1409905890@qq.com>
2023-03-23 19:25:52 +0800

    基础1

--- Assets/Scripts/ControllerFile/ViewController.cs
+++ Assets/Scripts/ControllerFile/ViewController.cs
@@ -1,36 +1,31 @@
 ﻿using System;
 //using TFramework;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class ViewController : MonoBehaviour
 {
-    const string INPUT_MOUSE_SCROLLWHEEL = "Mouse ScrollWheel";
-    const string INPUT_MOUSE_X = "Mouse X";
-    const string INPUT_MOUSE_Y = "Mouse Y";
+    private const string InputMouseScrollwheel = "Mouse ScrollWheel";
+    private const string InputMouseX = "Mouse X";
+    private const string InputMouseY = "Mouse Y";
+    private const string Horizontal = "Horizontal";
+    private const string Vertical = "Vertical";
+    public GameObject box;
 
-    public GameObject Box;
-    private GameObject verRotObj;
-    private GameObject horRotObj;
-    //鼠标增量
-    private float MouseX;
-    private float mouseY;
-    private float mHRot;
-    private float mVRot;
-    //鼠标灵敏度
-    public float mouseSensitivity;
-    //是否需要旋转
-    private bool isRevolve;
+    [Header("自动旋转速度")] public float autoRotSpeed;
+    [Header("移动速度")] public float posSpeed;
+    [Header("旋转速度")] public float rotSpeed;
+    [Header("限制上下旋转的角度")] 
+    public float rotUpLimit;
+    public float rotDownLimit;
 
-    [Header("最远查看距离限制")]
-    [Range(0, 1)]
-    public float farDistance = 0f;
-    [Header("最近查看距离限制")]
-    [Range(0, 1)]
-    public float nearDistance = 0.8f;
-    [Header("当前缩放距离")]
-    public float distance = 0.5f;
-    [Header("缩放速度")]
-  private float zoomSpeed1;
+    private float limitUp;
+    private float limitDown;
+    [Header("摄像机当前缩放距离")] public float objDistance;
+
+    
+
+    [Header("缩放速度")] private float zoomSpeed;
 
     //插值方法实现相机平滑放大缩小
     //相机缩放速度
@@ -46,29 +41,10 @@ public class ViewController : MonoBehaviour
     private float targetZoom;
     //平滑速度值
     private Vector3 smoothVelocity = Vector3.zero;
-    private void Start()
-    {  
-        //先获取当前平滑值存到TArgetzoom 
-        targetZoom = transform.position.magnitude;
-        //创建Box旋转需要的两个空物体
-        if (verRotObj == null)
-            verRotObj = new GameObject("VerRotObj");
-        if (horRotObj == null)
-            horRotObj = new GameObject("HorRotObj");
-        
-        InitBox();
-        initObjDistance();
-    }
 
-    /// <summary>
-    /// 初始化Box位置与层级关系
-    /// </summary>
-    private void InitBox()
+    private void Start()
     {
-        verRotObj.transform.position = Box.transform.position;
-        horRotObj.transform.position = Box.transform.position;
-        Box.transform.SetParent(horRotObj.transform);
-        horRotObj.transform.SetParent(verRotObj.transform);
+        initObjDistance();
     }
 
     /// <summary>
@@ -76,78 +52,72 @@ public class ViewController : MonoBehaviour
     /// 缩放速度改变
     /// </summary>
     private void initObjDistance()
-    {
+    { 
         //最大与最小范围随物体改变
-         minZoom = Box.transform.localScale.magnitude/2;
-         maxZoom =Box.transform.localScale.magnitude*2 ;
-         
-        zoomSpeed1 =maxZoom/2;
-       
+        minZoom = box.transform.localScale.magnitude/2;
+        maxZoom = box.transform.localScale.magnitude*2 ;
+        zoomSpeed =maxZoom/2;
     }
+
+    
 
     private void Update()
     {
-        //鼠标滚轮缩放未完成
-        UpdateObjDistanceState();
-        if(Input.GetMouseButton(0))
-            MouseButtonL();
-        if(Input.GetMouseButtonUp(0))
-            MouseButtonUpL();
+        //摄像机围绕物体旋转
+        CameraRotate();
         
-        BoxRotation();
+        //鼠标滚轮缩放
+        UpdateObjDistanceState();
+        //鼠标控制旋转
     }
 
     /// <summary>
-    /// 鼠标左键按下控制旋转
+    /// 摄像机围绕盒子旋转的方法
     /// </summary>
-    private void MouseButtonL()
+    private void CameraRotate()
     {
-        isRevolve = true;
-        //获得鼠标增量
-        MouseX = Input.GetAxis(INPUT_MOUSE_X);
-        mouseY = Input.GetAxis(INPUT_MOUSE_Y);
-        mHRot -= MouseX * mouseSensitivity * Time.deltaTime;
-        mVRot -= mouseY * mouseSensitivity * Time.deltaTime;
-        if(Mathf.Abs(MouseX) < 0.1f)
-            mHRot = Mathf.Lerp(mHRot, 0, 0.01f);
-        if(Mathf.Abs(mouseY) < 0.1f)
-            mVRot = Mathf.Lerp(mVRot, 0, 0.01f);
-    }
-
-    /// <summary>
-    /// 抬起鼠标左键时的操作
-    /// </summary>
-    private void MouseButtonUpL()
-    {
-        isRevolve = false;
-    }
-
-    /// <summary>
-    /// 盒子旋转
-    /// </summary>
-    public void BoxRotation()
-    {
-        verRotObj.transform.Rotate(new Vector3(mVRot, 0, 0), Space.Self); 
-        horRotObj.transform.Rotate(new Vector3(0, mHRot, 0), Space.Self);
-
-        if (!isRevolve)
+        transform.RotateAround(box.transform.position, Vector3.up, autoRotSpeed * Time.deltaTime); //摄像机自动围绕目标旋转
+        var mHROt = -Input.GetAxis(Horizontal);//获取键盘X轴移动
+        var mVROt = Input.GetAxis(Vertical);//获取键盘Y轴移动
+        var mouseX = Input.GetAxis(InputMouseX);//获取鼠标X轴移动
+        var mouseY = -Input.GetAxis(InputMouseY);//获取鼠标Y轴移动
+        limitUp = Mathf.Sin(rotUpLimit * Mathf.Deg2Rad) * objDistance;
+        limitDown = -Mathf.Sin(rotDownLimit * Mathf.Deg2Rad) * objDistance;
+        //按鼠标右键移动摄像机位置
+        if (Input.GetMouseButton(1))
         {
-            mHRot = Mathf.Lerp(mHRot, 0, 0.1f);
-            mVRot = Mathf.Lerp(mVRot, 0, 0.1f);
+            transform.Translate(Vector3.left * (mouseX * posSpeed * Time.deltaTime));
+            transform.Translate(Vector3.up * (mouseY * posSpeed * Time.deltaTime));
+        }
+        
+        //鼠标左键控制摄像机围绕物体旋转
+        //if (Input.GetMouseButton(0))
+        if(mHROt != 0 || mVROt != 0)
+        {
+            if ((transform.position.y >= limitUp && mVROt > 0) || (transform.position.y <= limitDown && mVROt < 0)) 
+                mVROt = 0;
+
+            transform.RotateAround(box.transform.transform.position, Vector3.up, mHROt * rotSpeed);
+            transform.RotateAround(box.transform.transform.position, transform.right, mVROt * rotSpeed);
+            
         }
     }
+
+    
 
     /// <summary>
     /// 鼠标滚轮缩放
     /// </summary>
     private void UpdateObjDistanceState()
     {
-        float scroll = Input.GetAxis(INPUT_MOUSE_SCROLLWHEEL);
-        targetZoom -= scroll * zoomSpeed1;
+        float scroll = Input.GetAxis(InputMouseScrollwheel);
+        targetZoom -= scroll * zoomSpeed;
         // 限制缩放范围
         targetZoom = Mathf.Clamp(targetZoom, minZoom, maxZoom);
         // 使用差值函数平滑缩放
         transform.position = Vector3.SmoothDamp(transform.position, transform.position.normalized * targetZoom, ref smoothVelocity, smoothTime);
+        // 获取当前缩放距离
+        objDistance = Vector3.Distance(transform.position, box.transform.position) - 100f;
     }
     
 }
---
+++ Assets/Scripts/ManagerFile/BoxObjManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class BoxObjManager : MonoBehaviour,IBeginDragHandler,IDragHandler,IEndDragHandler
+{
+    //鼠标开始拖动物体时的事件
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        
+    }
+
+    //拖动过程中的事件
+    public void OnDrag(PointerEventData eventData)
+    {
+        //物体的位置等于鼠标的位置
+        transform.position = eventData.position;
+    }
+
+    //拖动结束时的事件
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        
+    }
+}
