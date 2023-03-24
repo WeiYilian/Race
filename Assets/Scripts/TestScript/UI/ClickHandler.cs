27bc2e09519a93a97146fd9214c55e9c84be7273
aiyuan <1455881522@qq.com>
2023-03-24 16:52:38 +0800

    放大缩小单击双击

---
+++ Assets/Scripts/TestScript/UI/ClickHandler.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class ClickHandler : MonoBehaviour, IPointerClickHandler
+{
+    // 上一次点击的时间
+    private float lastClickTime = 0;
+
+    // 两次点击之间的最大时间间隔
+    private float doubleClickInterval = 0.3f;
+
+    // 单击事件回调函数
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (Time.time - lastClickTime < doubleClickInterval)
+        {
+            // 双击事件
+            Debug.Log("DoubleClick!");
+        }
+        else
+        {
+            // 单击事件
+            Debug.Log("SingleClick!");
+        }
+
+        // 更新上一次点击的时间
+        lastClickTime = Time.time;
+    }
+}
