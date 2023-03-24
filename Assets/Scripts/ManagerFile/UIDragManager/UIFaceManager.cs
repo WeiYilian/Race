6e04ec25d16e31afa3fa514d35245ce6544ed27e
WeiYilian <1409905890@qq.com>
2023-03-24 19:02:33 +0800

    写不下去了，头秃了

---
+++ Assets/Scripts/ManagerFile/UIDragManager/UIFaceManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.Serialization;
+using UnityEngine.UIElements;
+
+public class UIFaceManager : MonoBehaviour
+{
+   public static UIFaceManager Instance;
+   
+   public List<GameObject> UIFaceList;
+
+   private void Awake()
+   {
+      if (Instance == null)
+         Instance = this;
+   }
+}
