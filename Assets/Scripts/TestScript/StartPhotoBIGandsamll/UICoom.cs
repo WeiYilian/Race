7626846d63cceb98cbb05e1e107105d310eeb1fa
aiyuan <1455881522@qq.com>
2023-03-29 19:15:06 +0800

    photo放大缩小修正

--- Assets/Scripts/TestScript/StartPhotoBIGandsamll/UICoom.cs
+++ Assets/Scripts/TestScript/StartPhotoBIGandsamll/UICoom.cs
@@ -17,8 +17,8 @@ public class UICoom : MonoBehaviour, IPointerClickHandler
     private float doubleClickInterval = 0.3f;
     //放大系数
    
-    public Image parimage;
-    public Image image; // 要操作的图片
+   private GameObject parimage;//父物体
+    
     private RectTransform rectTrans; // 要操作的图片的RectTransform组件
     private Vector3 originalPosition; // 原始位置
     private Vector2 originalSize; // 原始大小
@@ -26,7 +26,8 @@ public class UICoom : MonoBehaviour, IPointerClickHandler
    
     void Start()
     {
-        rectTrans = image.GetComponent<RectTransform>(); // 获取RectTransform组件
+        parimage = gameObject.transform.parent.gameObject;
+        rectTrans = gameObject.GetComponent<RectTransform>(); // 获取RectTransform组件
         originalPosition = rectTrans.localPosition; // 记录原始位置
         originalSize = rectTrans.sizeDelta; // 记录原始大小
     }
@@ -35,41 +36,44 @@ public class UICoom : MonoBehaviour, IPointerClickHandler
     {if (Time.time - lastClickTime < doubleClickInterval)
         {
             // 双击事件
-            //下面的子物体
-            foreach(var child in image.GetComponentsInChildren<Transform>())
+            //下面的子物体缩小
+            if (isEnlarged==false)
             {
-                if(child != image.transform)
-                {
-                    child.localScale =child.localScale*originalSize.x/rectTrans.sizeDelta.x;
-                }
-            }
-            rectTrans.localPosition = originalPosition; // 缩小回原始位置
-            rectTrans.sizeDelta = originalSize; // 缩小回原始大小
-            isEnlarged = false;
-        }
-        else
-        {
-          
-            // 单击事件
-            if (!isEnlarged) // 当前未放大
-            {   
-                TextContorl.instance.str("ssssss");
                 rectTrans.localPosition = Vector3.zero; // 放大后中心点与UI面板重合
                 rectTrans.sizeDelta = parimage.GetComponent<RectTransform>().rect.size; // 放大后与UI面板一样大小
 
                 //缩小image下面的子物体按现在与之前的比值去放大
-                foreach (var child in image.GetComponentsInChildren<Transform>())
+                foreach (var child in gameObject.GetComponentsInChildren<Transform>())
                 {
-                    if (child != image.transform)
+                    if (child != gameObject.transform)
                     {
                         child.localScale = child.localScale * rectTrans.sizeDelta.x / originalSize.x;
                     }
                 }
-
                 isEnlarged = true;
             }
 
-            // 更新上一次点击的时间
+
+            else
+            {
+                foreach(var child in gameObject.GetComponentsInChildren<Transform>())
+                {
+                    if(child != gameObject.transform)
+                    {
+                        child.localScale =child.localScale*originalSize.x/rectTrans.sizeDelta.x;
+                    }
+                }
+                rectTrans.localPosition = originalPosition; // 缩小回原始位置
+                rectTrans.sizeDelta = originalSize; // 缩小回原始大小
+                isEnlarged = false;
+            }  
+            
+        }
+        else
+        {
+            
+
+            // 更新上一次点击的时间,记录点击时间
             lastClickTime = Time.time;
 
 
