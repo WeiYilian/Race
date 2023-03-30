59e13a4ab8512ae6da801fec182d86f6ea3ce51d
aiyuan <1455881522@qq.com>
2023-03-30 22:02:09 +0800

    第四面功能完成

--- Assets/Scripts/ManagerFile/FaceConcrete/Face4/SliderConter.cs
+++ Assets/Scripts/ManagerFile/FaceConcrete/Face4/SliderConter.cs
@@ -1,43 +1,61 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class SliderConter : MonoBehaviour
 {
-   private bool canPlayGame;
+    private static SliderConter _instance;
+   // private bool canPlayGame;
     //滑动温度在45-50即0.45到0.5之间可以触发按钮，否则按钮触发不了可以写一个状态当滑块到0.45到0.5时可以开始按下按钮
     // Start is called before the first frame update
-    
+   
 //一个判断是否滑动方法
-
+    private float value;
     private void Start()
     {
-        canPlayGame = false;
+      
+       
     }
 
-
-    public void OnValueChanged()
+    public static SliderConter Instance
     {
-      float  value = gameObject.transform.GetComponent<Slider>().value;
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new SliderConter();
+                
+            }
+            return _instance;
+        }
+    }
+   
+   public void OnValueChanged()
+    {
+        value = gameObject.transform.GetComponent<Slider>().value;
         if(value >= 40f && value <= 50f)
         {
-            canPlayGame = true;
-            Debug.Log("可以按下按钮");
+            
+            
+            
+            ButtenMana buttenMana = new ButtenMana();
+            
+            buttenMana.Game(true);
         }
         else
         {
-            canPlayGame = false;
-            Debug.Log("不能按下按钮");
+            ButtenMana buttenMana = new ButtenMana();
+            buttenMana.Game(false);
+           
         }
     }
 
     private void Update()
     {
         OnValueChanged();
+
     }
-public bool CanPlayGame
-    {
-       get{ return canPlayGame;}
-    }
+
 }
