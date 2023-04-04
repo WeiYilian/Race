2dabb6ee8fbdf241ca2f049667b86563b486b08c
aiyuan <1455881522@qq.com>
2023-04-04 22:35:36 +0800

    1

---
+++ Assets/Scripts/ManagerFile/FaceConcrete/Face3/Flow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flow : MonoBehaviour
+{
+    public bool bloom;
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Flow") //花洒
+        {
+            Debug.Log("ss");
+            if (!bloom)
+            { Debug.Log("s");
+                // animator.Play("Bloom");
+                animator.SetBool("Bloom", true);
+                bloom = true;
+            }
+        }
+    }
+}
+
+    
